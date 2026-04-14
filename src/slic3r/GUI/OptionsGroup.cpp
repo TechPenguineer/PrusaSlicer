TechPenguineer/PrusaSlicer

commit f18aa2ec13d273cd7de9aa723edecd859b1db35c

Fixed localization of tool-tips for "custom_parameters_*" parameters. + Beatified it's appearance in Print/FialmentSettings tabs.
(related to SPE-2953)

src/slic3r/GUI/OptionsGroup.cpp
@@ -276,6 +276,10 @@ Option::Option(const ConfigOptionDef& _opt, t_config_option_key id) : opt(_opt),
         if (opt.opt_key.rfind("branching", 0) == 0)
             tooltip = _L("Unavailable for this method.") + "\n";
         tooltip += _(opt.tooltip);
+        if (opt.opt_key.rfind("custom_parameters_", 0) == 0) {
+            //"custom_parameters_*" contains just a template tooltip which has to be formated after localization
+            tooltip = format_wxstr(tooltip, opt.opt_key);
+        }
 
         // edit tooltip : change Slic3r to SLIC3R_APP_KEY
         // Temporary workaround for localization

src/libslic3r/PrintConfig.cpp
@@ -2413,7 +2413,7 @@ void PrintConfigDef::init_fff_params()
           "The JSON must be single level (no arrays and objects). All value types are allowed, but nulls "
           "will be rejected by the parser."
     );
-    def->tooltip = (boost::format(custom_parameter_tooltip_templ) % "custom_parameter_print").str();
+    def->tooltip = custom_parameter_tooltip_templ;
     def->multiline = true;
     def->full_width = true;
     def->height = 13;
@@ -3110,7 +3110,7 @@ void PrintConfigDef::init_fff_params()
 
     def = this->add("custom_parameters_printer", coString);
     def->label = L("Custom printer parameters");
-    def->tooltip = (boost::format(custom_parameter_tooltip_templ) % "custom_parameter_printer").str();;
+    def->tooltip = custom_parameter_tooltip_templ;
     def->multiline = true;
     def->full_width = true;
     def->height = 12;
@@ -3129,7 +3129,7 @@ void PrintConfigDef::init_fff_params()
 
     def = this->add("custom_parameters_filament", coStrings);
     def->label = L("Custom filament parameters");
-    def->tooltip = (boost::format(custom_parameter_tooltip_templ) % "custom_parameter_filament").str();
+    def->tooltip = custom_parameter_tooltip_templ;
     def->multiline = true;
     def->full_width = true;
     def->height = 12;

src/slic3r/GUI/Tab.cpp
@@ -1717,7 +1717,7 @@ void TabPrint::build()
         optgroup->append_single_option_line("min_bead_width");
         optgroup->append_single_option_line("min_feature_size");
 
-        optgroup = page->new_optgroup(L("Custom parameters"));
+        optgroup = page->new_optgroup(L("Custom parameters"), 0);
         auto option = optgroup->get_option("custom_parameters_print");
         option.opt.is_code = true;
         optgroup->append_single_option_line(option);
@@ -2382,7 +2382,7 @@ void TabFilament::build()
         option.opt.height = gcode_field_height;// 150;
         optgroup->append_single_option_line(option);
 
-        optgroup = page->new_optgroup(L("Custom parameters"));
+        optgroup = page->new_optgroup(L("Custom parameters"), 0);
         option = optgroup->get_option("custom_parameters_filament");
         option.opt.is_code = true;
         optgroup->append_single_option_line(option);

commit a3d56915f2ab78eab56888192643ab46692c7dc0

SPE-3637: Fixed incorrect tooltip on custom_parameters_* (#15033), broken in f18aa2e

src/slic3r/GUI/OptionsGroup.cpp
@@ -20,6 +20,7 @@
 #include <wx/bookctrl.h> // IWYU pragma: keep
 #include <wx/numformatter.h>
 #include <boost/algorithm/string/split.hpp>
+#include <boost/algorithm/string/replace.hpp>
 #include <boost/algorithm/string/classification.hpp>
 #include "slic3r/GUI/Search.hpp" // IWYU pragma: keep
 #include "slic3r/GUI/Field.hpp"
@@ -276,9 +277,14 @@ Option::Option(const ConfigOptionDef& _opt, t_config_option_key id) : opt(_opt),
         if (opt.opt_key.rfind("branching", 0) == 0)
             tooltip = _L("Unavailable for this method.") + "\n";
         tooltip += _(opt.tooltip);
-        if (opt.opt_key.rfind("custom_parameters_", 0) == 0) {
+        if (boost::starts_with(opt.opt_key, "custom_parameters_")) {
             //"custom_parameters_*" contains just a template tooltip which has to be formated after localization
-            tooltip = format_wxstr(tooltip, opt.opt_key);
+            // Note that the tooltip should use singular form (parameters -> parameter), because that is how the parameters
+            // are addressed (https://github.com/prusa3d/PrusaSlicer/issues/15033). This is fragile, but it might do.
+            if (tooltip.find("%1%") == wxString::npos)
+                std::terminate(); // this is a programmer error, the tooltip should contain "%1%" placeholder for parameter name
+            const std::string param_prefix = boost::replace_first_copy(opt.opt_key, "custom_parameters_", "custom_parameter_");
+            tooltip = format_wxstr(tooltip, param_prefix);
         }
 
         // edit tooltip : change Slic3r to SLIC3R_APP_KEY